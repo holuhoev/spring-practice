8648b9de853d82d6debce62f38308b81673dd51b
Evgeny Holuhoev <kholukhoev.e.eld@sberbank.ru>
2020-09-21 19:47:31 +0300

    Init commit

---
+++ context/src/main/java/ru/holuhoev/spring_practice/context/ContextPracticeApplication.java
@@ -0,0 +1,12 @@
+package ru.holuhoev.spring_practice.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextPracticeApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ContextPracticeApplication.class, args);
+    }
+}
